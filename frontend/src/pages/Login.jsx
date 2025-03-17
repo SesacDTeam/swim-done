94b25b93b2eba632619bc995bbe3f7836863860d
ssdeok <bechu123456@gmail.com>
2025-03-17 21:15:41 +0900

    [SW-17] feat: 카카오톡 소셜로그인 버튼 구현 (#9)

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import KakaoLoginButton from '../components/sociallogin/KakaoLoginButton';
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <>
+      <div>
+        <h1>
+          흩어져있던 <span style={{ color: '#0085ff' }}>자유 수영 정보</span>를 한눈에!
+        </h1>
+        <h1>오수완에 오신 걸 환영해요.</h1>
+      </div>
+      <KakaoLoginButton></KakaoLoginButton>
+    </>
+  );
 }
