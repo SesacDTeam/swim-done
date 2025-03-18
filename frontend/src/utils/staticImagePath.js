44f3843eb17961e2133f13c79d010c9f5dfe3d43
Hamo <85005933+lxodud@users.noreply.github.com>
2025-03-18 11:39:08 +0900

    [SW-27] 공통 css 이미지 세팅 (#13)
    * [SW-27] design: 아이콘 추가
    
    * [SW-27] chore: design 커밋 컨벤션 필터 추가
    
    * [SW-27] design: staticImagePath 구현
    
    * [SW-27] design: staticImagePath 구현
    
    * [SW-27] chore: 프론트 최상위 구조 수정
    
    * [SW-27] design: 전역 컬러, 폰트 구현
    
    * [SW-27] design: blue02 색 수정

---
+++ frontend/src/utils/staticImagePath.js
@@ -0,0 +1,31 @@
+import logo from '../assets/logo.svg';
+import home from '../assets/home.svg';
+import contactUs from '../assets/contactUs.svg';
+import kewordReview from '../assets/keywordReview.svg';
+import markPool from '../assets/markPool.svg';
+import mark from '../assets/mark.svg';
+import more from '../assets/more.svg';
+import myReview from '../assets/myReview.svg';
+import mypage from '../assets/mypage.svg';
+import noContent from '../assets/noContent.svg';
+import pictureColor from '../assets/pictureColor.svg';
+import profile from '../assets/profile.svg';
+import swimming from '../assets/swimming.svg';
+import wave from '../assets/wave.svg';
+
+export default {
+  logo,
+  home,
+  contactUs,
+  kewordReview,
+  markPool,
+  mark,
+  more,
+  myReview,
+  mypage,
+  noContent,
+  pictureColor,
+  profile,
+  swimming,
+  wave,
+};
