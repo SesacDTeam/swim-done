2cd416bba6405d5d48853206e9310039b81c80e1
kdh <kdh462601@gmail.com>
2025-03-18 14:54:56 +0900

    [SW-23] feat: Week정보 한글형태로 출력할 수 있도록 ㅗ드 수정

--- backend/src/main/java/com/done/swim/domain/swimmingtime/dto/FreeSwimmingTimeResponseDto.java
+++ backend/src/main/java/com/done/swim/domain/swimmingtime/dto/FreeSwimmingTimeResponseDto.java
@@ -1,7 +1,6 @@
 package com.done.swim.domain.swimmingtime.dto;
 
 import com.done.swim.domain.swimmingtime.entity.SwimmingTime;
-import com.done.swim.domain.swimmingtime.entity.Week;
 import java.time.LocalTime;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,13 +11,13 @@ public class FreeSwimmingTimeResponseDto {
 
   private LocalTime startTime;
   private LocalTime endTime;
-  private Week dayOfWeek;
+  private String dayOfWeek;
 
   public static FreeSwimmingTimeResponseDto from(SwimmingTime entity) {
     return FreeSwimmingTimeResponseDto.builder()
         .startTime(entity.getStartTime())
         .endTime(entity.getEndTime())
-        .dayOfWeek(entity.getDayOfWeek())
+        .dayOfWeek(entity.getDayOfWeek().getKoreanName())
         .build();
   }
 }
