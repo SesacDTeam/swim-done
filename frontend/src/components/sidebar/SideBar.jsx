c3eb3dea64dd1bcacf2d8c2ea687448a5f046b3e
lxodud <xodud1998223@naver.com>
2025-03-18 15:28:42 +0900

    [SW-20] design: SideBar UI 구현

--- frontend/src/components/sidebar/SideBar.jsx
+++ frontend/src/components/sidebar/SideBar.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import staticImagePath from '../../utils/staticImagePath';
+import SideBarItem from './SideBarItem';
 
 export default function SideBar() {
-  return <div>SideBar</div>;
+  return (
+    <>
+      <div className="border-gray03 border-[0.5px] w-30 flex flex-col items-center h-full">
+        <img src={staticImagePath.logo} alt="" className="w-2/3 mt-5" />
+        <div className="h-[0.5px] w-full bg-gray03 mt-[20px]"></div>
+
+        <SideBarItem
+          image={staticImagePath.mypage}
+          selectedImage={staticImagePath.mypageColor}
+          title="마이페이지"
+          isSelected={false}
+        ></SideBarItem>
+        <SideBarItem
+          image={staticImagePath.markPool}
+          selectedImage={staticImagePath.markPoolColor}
+          title="내가 찜한 수영장"
+          isSelected={true}
+        ></SideBarItem>
+      </div>
+    </>
+  );
 }
