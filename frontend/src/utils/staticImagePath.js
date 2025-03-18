00b15e77423b06f759439791e19793dc63f4e199
lxodud <xodud1998223@naver.com>
2025-03-18 15:05:24 +0900

    [SW-20] design: 이미지 수정 및 추가

--- frontend/src/utils/staticImagePath.js
+++ frontend/src/utils/staticImagePath.js
@@ -1,28 +1,41 @@
 import logo from '../assets/logo.svg';
 import home from '../assets/home.svg';
 import contactUs from '../assets/contactUs.svg';
+import contactUsColor from '../assets/contactUs.svg'
 import kewordReview from '../assets/keywordReview.svg';
+import kewordReviewColor from '../assets/keywordReviewColor.svg';
 import markPool from '../assets/markPool.svg';
+import markPoolColor from '../assets/markPoolColor.svg';
 import mark from '../assets/mark.svg';
+import markColor from '../assets/markColor.svg';
 import more from '../assets/more.svg';
 import myReview from '../assets/myReview.svg';
+import myReviewColor from '../assets/myReviewColor.svg';
 import mypage from '../assets/mypage.svg';
+import mypageColor from '../assets/mypageColor.svg';
 import noContent from '../assets/noContent.svg';
 import pictureColor from '../assets/pictureColor.svg';
 import profile from '../assets/profile.svg';
 import swimming from '../assets/swimming.svg';
 import wave from '../assets/wave.svg';
 
+
 export default {
   logo,
   home,
   contactUs,
+  contactUsColor,
   kewordReview,
+  kewordReviewColor,
   markPool,
+  markPoolColor,
   mark,
+  markColor,
   more,
   myReview,
+  myReviewColor,
   mypage,
+  mypageColor,
   noContent,
   pictureColor,
   profile,
