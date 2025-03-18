2b217b1eb4d543e6b9a56cd6ff2066b8e8d372cd
lxodud <xodud1998223@naver.com>
2025-03-18 10:26:46 +0900

    [SW-27] design: staticImagePath 구현

---
+++ frontend/src/utils/staticImagePath.js
@@ -0,0 +1,31 @@
+import logo from '../assets/logo.svg';
+import home from '../assets/logo.svg';
+import contactUs from '../assets/contact_us.svg';
+import kewordReview from '../assets/keyword_review.svg';
+import markPool from '../assets/mark_pool.svg';
+import mark from '../assets/mark.svg';
+import more from '../assets/more.svg';
+import myReview from '../assets/my_review.svg';
+import mypage from '../assets/mypage.svg';
+import noContent from '../assets/no_content.svg';
+import pictureColor from '../assets/pictureColor.svg';
+import profile from '../assets/profile.svg';
+import swimming from '../assets/swimming.svg';
+import wave from '../assets/wave.svg';
+
+export default {
+  logo,
+  home,
+  contactUs,
+  kewordReview,
+  markPool,
+  mark,
+  more,
+  myReview,
+  mypage,
+  noContent,
+  pictureColor,
+  profile,
+  swimming,
+  wave,
+};
