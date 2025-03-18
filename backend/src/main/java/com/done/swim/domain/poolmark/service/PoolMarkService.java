a906c2d6b2c27e5cf4f9e29eeade2262c2f8121a
lxodud <xodud1998223@naver.com>
2025-03-18 21:04:18 +0900

    [SW-36] feat: PoolMarkService 찜 목록 조회 메서드 구현

--- backend/src/main/java/com/done/swim/domain/poolmark/service/PoolMarkService.java
+++ backend/src/main/java/com/done/swim/domain/poolmark/service/PoolMarkService.java
@@ -2,12 +2,15 @@ package com.done.swim.domain.poolmark.service;
 
 import com.done.swim.domain.pool.entity.Pool;
 import com.done.swim.domain.pool.repository.PoolRepository;
+import com.done.swim.domain.poolmark.dto.PoolMarkListResponseDto;
 import com.done.swim.domain.poolmark.entity.PoolMark;
 import com.done.swim.domain.poolmark.repository.PoolMarkRepository;
 import com.done.swim.domain.user.entity.User;
 import com.done.swim.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -37,5 +40,11 @@ public class PoolMarkService {
 
     PoolMark poolMark = PoolMark.builder().user(user).pool(pool).build();
     poolMarkRepository.save(poolMark);
+  }
+
+  public PoolMarkListResponseDto getMyPoolMark(Pageable pageable) {
+    Page<PoolMark> poolMarkPage = poolMarkRepository.findByUserId(2L, pageable);
+
+    return PoolMarkListResponseDto.from(poolMarkPage);
   }
 }
