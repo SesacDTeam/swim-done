c9a1f08c93b3faa432b7bc720e7bdb0bb184206e
lxodud <xodud1998223@naver.com>
2025-03-19 18:13:48 +0900

    [SW-48] design: SideBar 위치 css 구현

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,8 +8,12 @@ export default function Home() {
     <>
       <div className="flex h-full">
         <SideBar></SideBar>
-        <KakaoMapContainer></KakaoMapContainer>
-        <Outlet></Outlet>
+        <div className='grow relative'>
+          <KakaoMapContainer></KakaoMapContainer>
+          <section className="absolute left-0 top-0 bottom-0 w-100 z-3000 bg-white">
+            <Outlet></Outlet>
+          </section>
+        </div>
       </div>
     </>
   );
