7148ad42a90053ef3ab8aa932761c5b11b8743e1
lxodud <xodud1998223@naver.com>
2025-03-20 22:22:59 +0900

    [SW-48] feat: markPoolApi 객체 구현

---
+++ frontend/src/api/markPoolApi.js
@@ -0,0 +1,33 @@
+import instance from './axios';
+
+export const markPoolApi = {
+  getMyMarkedPools: async (token) => {
+    const response = await instance.get('/my/marks', {
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    });
+
+    return response.data;
+  },
+
+  createMarkedPools: async (token, poolId) => {
+    const response = await instance.post(`/marks/pools/${poolId}`, {}, {
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    });
+
+    return response.data;
+  },
+
+  deleteMyMarkedPools: async (token, poolId) => {
+    const response = await instance.delete(`/marks/pools/${poolId}`, {
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    });
+
+    return response.data;
+  },
+};
