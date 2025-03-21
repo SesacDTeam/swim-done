358042d736bdfbb7228635da20ceb32d8ab156f6
lxodud <xodud1998223@naver.com>
2025-03-21 17:19:15 +0900

    [SW-58] feat: DetailViewHeader 컴포넌트 구현

---
+++ frontend/src/components/common/DetailViewHeader.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router';
+import { hideDetailView } from '../../store/slices/detailViewSlice';
+
+export default function DetailViewHeader({ leftButtonImage, rightButtonImage }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <header className="mx-8 mt-8 flex justify-between">
+        {leftButtonImage ? (
+          <img
+            src={leftButtonImage}
+            alt=""
+            className="w-8 aspect-square cursor-pointer"
+            onClick={() => {
+              navigate(-1);
+            }}
+          />
+        ) : (
+          <div className="w-8 aspect-square"></div>
+        )}
+        <img
+          src={rightButtonImage}
+          alt=""
+          className="w-7 aspect-square cursor-pointer"
+          onClick={() => dispatch(hideDetailView())}
+        />
+      </header>
+    </>
+  );
+}
