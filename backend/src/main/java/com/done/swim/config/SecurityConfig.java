4d0d6e2c6fd3a1e555532754d90f66a6796b4528
lxodud <xodud1998223@naver.com>
2025-03-22 15:49:53 +0900

    [SW-58] fix: 수영장 디테일 조회 api 허용 추가

--- backend/src/main/java/com/done/swim/config/SecurityConfig.java
+++ backend/src/main/java/com/done/swim/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -26,67 +27,68 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final CustomAccessDeniedHandler accessDeniedHandler;
-    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-    private final CustomOAuth2UserService customOAuth2UserService;
-    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
-    private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
+  private final CustomAccessDeniedHandler accessDeniedHandler;
+  private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+  private final CustomOAuth2UserService customOAuth2UserService;
+  private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+  private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
 
-    @Value("${origin}")
-    private String origin;
+  @Value("${origin}")
+  private String origin;
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/verify").authenticated()
-                        .requestMatchers("/auth/**", "/error", "/images/**").permitAll()
-                        .requestMatchers("/", "/login/**").permitAll()
-                        .requestMatchers("/oauth2/**", "/login/oauth2/code/**").permitAll()
-                        .requestMatchers("/login/oauth2/", "/oauth2/authorization/**").permitAll()
-                        .requestMatchers("/api/sections").permitAll()
-                        .requestMatchers("/login-success").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .oauth2Login(oauth2 -> oauth2
-                        .loginPage("/login-success")  // 추가
-                        .authorizationEndpoint(endpoint ->
-                                endpoint.baseUri("/oauth2/authorization")
-                        )
-                        .redirectionEndpoint(endpoint ->
-                                endpoint.baseUri("/login/oauth2/code/*")
-                        )
-                        .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
-                        .successHandler(oAuth2LoginSuccessHandler)
-                        .failureHandler(oAuth2LoginFailureHandler)
-                )
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .exceptionHandling(exception -> exception
-                        .accessDeniedHandler(accessDeniedHandler)
-                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-                );
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+      .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+      .csrf(csrf -> csrf.disable())
+      .sessionManagement(session -> session
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+      .authorizeHttpRequests(auth -> auth
+        .requestMatchers("/auth/verify").authenticated()
+        .requestMatchers("/auth/**", "/error", "/images/**").permitAll()
+        .requestMatchers("/", "/login/**").permitAll()
+        .requestMatchers("/oauth2/**", "/login/oauth2/code/**").permitAll()
+        .requestMatchers("/login/oauth2/", "/oauth2/authorization/**").permitAll()
+        .requestMatchers("/api/sections").permitAll()
+        .requestMatchers(HttpMethod.GET, "/api/pools/**").permitAll()
+        .requestMatchers("/login-success").permitAll()
+        .anyRequest().authenticated()
+      )
+      .oauth2Login(oauth2 -> oauth2
+        .loginPage("/login-success")  // 추가
+        .authorizationEndpoint(endpoint ->
+          endpoint.baseUri("/oauth2/authorization")
+        )
+        .redirectionEndpoint(endpoint ->
+          endpoint.baseUri("/login/oauth2/code/*")
+        )
+        .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
+        .successHandler(oAuth2LoginSuccessHandler)
+        .failureHandler(oAuth2LoginFailureHandler)
+      )
+      .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+      .exceptionHandling(exception -> exception
+        .accessDeniedHandler(accessDeniedHandler)
+        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+      );
 
-        return http.build();
-    }
+    return http.build();
+  }
 
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin(origin);
-        configuration.addAllowedMethod("*");
-        configuration.addAllowedHeader("*");
-        configuration.setAllowCredentials(true);
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.addAllowedOrigin(origin);
+    configuration.addAllowedMethod("*");
+    configuration.addAllowedHeader("*");
+    configuration.setAllowCredentials(true);
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
 
 
 }
