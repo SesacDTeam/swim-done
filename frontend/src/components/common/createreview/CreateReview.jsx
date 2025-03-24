278a0829bf0be9f00aebf655cddc0d703ac108fe
hisunny1007 <hisunny1007@gmail.com>
2025-03-24 22:15:54 +0900

    [SW-59] refactor: 수영장 제목 가져오는 방법 수정 (uselocation 사용)
    - PoolDetail의 리뷰 쓰기 버튼 Link에 상태(state) 추가
    - CreateReview에서 state 전달받음 (uselocation 사용)

--- frontend/src/components/common/createreview/CreateReview.jsx
+++ frontend/src/components/common/createreview/CreateReview.jsx
@@ -1,66 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router';
+import React, { useState } from 'react';
+import { useLocation, useNavigate, useParams } from 'react-router';
 import DetailViewHeader from '../DetailViewHeader';
-import { poolApi } from '../../../api/poolApi';
 import { xmark, back } from '../../../utils/staticImagePath';
+import reviewApi from '../../../api/reviewApi';
 
 export default function CreateReview() {
+
   const [error, setError] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const { poolId } = useParams();
-  const [poolDetail, setPoolDetail] = useState();
+  const navigate = useNavigate();
 
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   setError('');
-  //   setIsLoading(true);
+  const location = useLocation();
+  const poolName = location.state?.poolName || "수영장 없음"; 
 
-  //   try {
-  //     const response = await authApi.login(formData);
-  //     const data = response.data;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  //     const { token } = data.data;
-  //     dispatch(login(token));
-  //     alert('로그인 성공');
-  //     navigate('/calendar');
-  //   } catch (err) {
-  //     setError(err.response.data.message);
-  //   } finally {
-  //     setIsLoading(false);
-  //   }
-  // };
+    try {
+      // const response = await reviewApi.createReview(poolId);
+      navigate(`/mark-pools/${poolId}`);
+      alert('리뷰작성 성공');
+    } catch (err) {
+      setError(err.message);
+    }
+  };
 
-  // const navigate = useNavigate();
-
-  // const toDetail = () => {
-  //   navigate('/details');
-  // };
-  
-  useEffect(() => {
-    (async () => {
-      // TODO: 로딩 처리, 에러 핸들링
-      const data = await poolApi.getPoolDetail(poolId);
-      const poolDetailData = data.data;
-      setPoolDetail(poolDetailData);
-    })();
-  }, [poolId]);
 
   return (
     <>
       <main className="flex flex-col items-center w-full">
         <DetailViewHeader backButtonImage={back} closeButtonImage={xmark}></DetailViewHeader>
         <section className="w-[80%] flex flex-col items-center mb-10">
-          <h1 className="pretendard-bold text-3xl">{poolDetail?.name}</h1>
+          <h1 className="pretendard-bold text-3xl">{poolName}</h1>
         </section>
 
-        <section className='w-150 mt-30 flex flex-col'>
-          <textarea
-            className="pretendard-medium text-xl w-full h-50 border border-gray-400 rounded-lg p-4 mb-4 focus:outline-none focus:ring-2 focus:ring-blue02 transition-all resize-none"
-            id="createReview"
-            name="createReview"
-            placeholder="실제 이용하신 후기를 자유롭게 남겨 주세요."
-          >
-          </textarea>
+        <section className="w-150 mt-30 flex flex-col">
+          <form onSubmit={handleSubmit}>
+            <textarea
+              className="pretendard-medium text-xl w-full h-50 border border-gray04 rounded-lg p-4 mb-4 focus:outline-none focus:ring-2 focus:ring-blue02 transition-all resize-none"
+              id="createReview"
+              name="createReview"
+              placeholder="실제 이용하신 후기를 자유롭게 남겨 주세요."
+            ></textarea>
+            <button className="pretendard-medium text-xl bg-blue02/10 rounded-[10px] px-4 py-2 mt-4 float-right">
+              제출
+            </button>
+          </form>
+          {/* TODO: input태그랑 textarea태그 중에 어울리는 것 결정 예정 */}
           {/* <form>
             <input
               className="pretendard-medium text-xl w-full border-b border-gray-400 p-2 mb-2 focus:outline-none"
@@ -70,11 +57,6 @@ export default function CreateReview() {
               placeholder="실제 이용하신 후기를 자유롭게 남겨 주세요."
             />
           </form> */}
-          <button
-            className="pretendard-medium text-xl bg-blue02/10 rounded-[10px] px-4 py-2 mt-4 self-end"
-            type="submit">
-            제출
-          </button>
         </section>
       </main>
     </>
