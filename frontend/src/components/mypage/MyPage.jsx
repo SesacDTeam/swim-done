432c843ec999666b7b066f9a5bc02caba14acd3f
kdh <kdh462601@gmail.com>
2025-03-24 10:31:54 +0900

    [SW-61] feat: css 스타일 수정, Mouse이벤트 관련 component 추가

--- frontend/src/components/mypage/MyPage.jsx
+++ frontend/src/components/mypage/MyPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import HoverItem from './HoverItem';
 
 import {
   profile,
@@ -11,134 +11,42 @@ import {
 } from '../../utils/staticImagePath';
 
 export default function MyPage() {
-  const [verification, setVerification] = useState({
-    myReviewVerification: false,
-    keywordReviewVerification: false,
-    contactUsVerification: false,
-  });
-
   return (
-    <>
-      <div className="select-none">
-        <h1 className="pretendard-bold text-2xl mt-10 ml-5 sticky text-center">마이페이지</h1>
-        <div className="flex justify-between relative top-18 h-26 w-85 mx-auto">
-          <div className="flex flex-col justify-between h-full">
-            <div>
-              <div className="text-2xl pretendard-bold mb-1">닉네임 님</div>
-              <div className="text-2xl pretendard-bold text-blue-500">오늘도 즐수하세요!</div>
-            </div>
-            <div className="-5 pretendard-medium">swim-done@kakao.com</div>
-          </div>
+    <div className="select-none">
+      <h1 className="pretendard-bold text-2xl mt-10 ml-5 sticky text-center">마이페이지</h1>
+      <div className="flex justify-between relative top-18 h-26 w-85 mx-auto">
+        <div className="flex flex-col justify-between h-full">
           <div>
-            <div className="h-17 w-20 rounded-full overflow-hidden flex items-center justify-center">
-              <img src={profile} alt="" className="h-full w-full" />
-            </div>
-            <div className=" text-center cursor-pointer">로그아웃</div>
+            <div className="text-2xl pretendard-bold mb-1">닉네임 님</div>
+            <div className="text-2xl pretendard-bold text-blue01">오늘도 즐수하세요!</div>
           </div>
+          <div>swim-done@kakao.com</div>
         </div>
-
-        <div className=" relative top-30 h-80 w-85 mx-auto flex flex-col justify-between">
-          <div className=" flex h-20 items-center">
-            <div>
-              <img
-                src={verification.myReviewVerification ? myReviewColor : myReview}
-                alt=""
-                className="w-20 h-15 cursor-pointer"
-                onMouseEnter={() =>
-                  setVerification({ ...verification, myReviewVerification: true })
-                }
-                onMouseLeave={() =>
-                  setVerification({ ...verification, myReviewVerification: false })
-                }
-              />
-            </div>
-            <div
-              className="relative ml-8 cursor-pointer"
-              onMouseEnter={() => setVerification({ ...verification, myReviewVerification: true })}
-              onMouseLeave={() => setVerification({ ...verification, myReviewVerification: false })}
-            >
-              {verification.myReviewVerification ? (
-                <div className="text-2xl text-[#69b5ff]  w-full ">내가 남긴 리뷰</div>
-              ) : (
-                <div className="text-2xl w-full ">내가 남긴 리뷰</div>
-              )}
-            </div>
+        <div>
+          <div className="h-17 w-20 rounded-full overflow-hidden flex items-center justify-center">
+            <img src={profile} alt="" className="h-full w-full" />
           </div>
-          <div className=" flex h-20 items-center">
-            <div>
-              <img
-                src={verification.keywordReviewVerification ? keywordReviewColor : keywordReview}
-                alt=""
-                className="w-20 h-15 cursor-pointer"
-                onMouseEnter={() =>
-                  setVerification({ ...verification, keywordReviewVerification: true })
-                }
-                onMouseLeave={() =>
-                  setVerification({ ...verification, keywordReviewVerification: false })
-                }
-              />
-            </div>
-            <div
-              className="relative ml-8 cursor-pointer"
-              onMouseEnter={() =>
-                setVerification({ ...verification, keywordReviewVerification: true })
-              }
-              onMouseLeave={() =>
-                setVerification({ ...verification, keywordReviewVerification: false })
-              }
-            >
-              {verification.keywordReviewVerification ? (
-                <div className="text-2xl text-[#69b5ff]  w-full ">키워드리뷰</div>
-              ) : (
-                <div className="text-2xl w-full ">키워드리뷰</div>
-              )}
-            </div>
-          </div>
-          <div className=" flex h-20 items-center">
-            <div>
-              <img
-                src={verification.contactUsVerification ? contactUsColor : contactUs}
-                alt=""
-                className="w-20 h-15 cursor-pointer"
-                onClick={() =>
-                  window.open(
-                    'https://docs.google.com/forms/d/11e9EqdCulELLjuw7oIisCIXd_DCb_XJBDnTsSJBnjPE/edit',
-                    '_blank',
-                  )
-                }
-                onMouseEnter={() =>
-                  setVerification({ ...verification, contactUsVerification: true })
-                }
-                onMouseLeave={() =>
-                  setVerification({ ...verification, contactUsVerification: false })
-                }
-              />
-            </div>
-            <div
-              className="relative ml-8 cursor-pointer"
-              onClick={() =>
-                window.open(
-                  'https://docs.google.com/forms/d/11e9EqdCulELLjuw7oIisCIXd_DCb_XJBDnTsSJBnjPE/edit',
-                  '_blank',
-                )
-              }
-              onMouseEnter={() => setVerification({ ...verification, contactUsVerification: true })}
-              onMouseLeave={() =>
-                setVerification({ ...verification, contactUsVerification: false })
-              }
-            >
-              {verification.contactUsVerification ? (
-                <div className="text-2xl text-[#69b5ff]  w-full ">문의하기</div>
-              ) : (
-                <div className="text-2xl w-full ">문의하기</div>
-              )}
-            </div>
-          </div>
-        </div>
-        <div className="relative top-67 h-10 w-85 mx-auto flex justify-center items-center">
-          <span className="cursor-pointer">회원 탈퇴하기</span>
+          <div className=" text-center cursor-pointer">로그아웃</div>
         </div>
       </div>
-    </>
+      <div className="relative top-30 h-80 w-85 mx-auto flex flex-col justify-between">
+        <HoverItem image={myReview} hoverImage={myReviewColor} text="내가 남긴 리뷰" />
+        <HoverItem image={keywordReview} hoverImage={keywordReviewColor} text="키워드리뷰" />
+        <HoverItem
+          image={contactUs}
+          hoverImage={contactUsColor}
+          text="문의하기"
+          onClick={() =>
+            window.open(
+              'https://docs.google.com/forms/d/11e9EqdCulELLjuw7oIisCIXd_DCb_XJBDnTsSJBnjPE/edit',
+              '_blank',
+            )
+          }
+        />
+      </div>
+      <div className="relative top-67 h-10 w-85 mx-auto flex justify-center items-center">
+        <span className="cursor-pointer">회원 탈퇴하기</span>
+      </div>
+    </div>
   );
 }
