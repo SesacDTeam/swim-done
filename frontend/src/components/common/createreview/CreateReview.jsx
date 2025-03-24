57ac4049dc137b7d3721d3880c899917e5a038d8
hisunny1007 <hisunny1007@gmail.com>
2025-03-24 17:32:31 +0900

    [SW-59] refactor: input -> textarea로 변경
    - 한 줄 리뷰가 아니기 때문에 직관적으로 리뷰 작성을 보여주기 위해 textarea로 변경
    - 그에 맞는 css 적용

--- frontend/src/components/common/createreview/CreateReview.jsx
+++ frontend/src/components/common/createreview/CreateReview.jsx
@@ -10,6 +10,32 @@ export default function CreateReview() {
   const { poolId } = useParams();
   const [poolDetail, setPoolDetail] = useState();
 
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   setError('');
+  //   setIsLoading(true);
+
+  //   try {
+  //     const response = await authApi.login(formData);
+  //     const data = response.data;
+
+  //     const { token } = data.data;
+  //     dispatch(login(token));
+  //     alert('로그인 성공');
+  //     navigate('/calendar');
+  //   } catch (err) {
+  //     setError(err.response.data.message);
+  //   } finally {
+  //     setIsLoading(false);
+  //   }
+  // };
+
+  // const navigate = useNavigate();
+
+  // const toDetail = () => {
+  //   navigate('/details');
+  // };
+  
   useEffect(() => {
     (async () => {
       // TODO: 로딩 처리, 에러 핸들링
@@ -27,8 +53,15 @@ export default function CreateReview() {
           <h1 className="pretendard-bold text-3xl">{poolDetail?.name}</h1>
         </section>
 
-        <section className='w-150 mt-45 flex flex-col'>
-          <form>
+        <section className='w-150 mt-30 flex flex-col'>
+          <textarea
+            className="pretendard-medium text-xl w-full h-50 border border-gray-400 rounded-lg p-4 mb-4 focus:outline-none focus:ring-2 focus:ring-blue02 transition-all resize-none"
+            id="createReview"
+            name="createReview"
+            placeholder="실제 이용하신 후기를 자유롭게 남겨 주세요."
+          >
+          </textarea>
+          {/* <form>
             <input
               className="pretendard-medium text-xl w-full border-b border-gray-400 p-2 mb-2 focus:outline-none"
               type="text"
@@ -36,7 +69,7 @@ export default function CreateReview() {
               name="createReview"
               placeholder="실제 이용하신 후기를 자유롭게 남겨 주세요."
             />
-          </form>
+          </form> */}
           <button
             className="pretendard-medium text-xl bg-blue02/10 rounded-[10px] px-4 py-2 mt-4 self-end"
             type="submit">
