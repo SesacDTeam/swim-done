c394f51f53154b87cd41ad0a150bf05e9a9c61d0
kdh <kdh462601@gmail.com>
2025-03-24 11:28:40 +0900

    [SW-61] refactor: 동적 데이터 저장할 useState 추가

--- frontend/src/components/mypage/MyPage.jsx
+++ frontend/src/components/mypage/MyPage.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import HoverItem from './HoverItem';
 
 import {
@@ -10,23 +11,35 @@ import {
   contactUsColor,
 } from '../../utils/staticImagePath';
 
+function logout() {
+  alert('로그아웃');
+}
+
+function removeUser() {
+  alert('회원탈퇴');
+}
+
 export default function MyPage() {
+  const [nickName, setNickName] = useState('');
+  const [emain, setEmain] = useState('');
+  const [userImg, setUserImg] = useState('');
+
   return (
     <div className="select-none">
       <h1 className="pretendard-bold text-2xl mt-10 ml-5 sticky text-center">마이페이지</h1>
       <div className="flex justify-between relative top-18 h-26 w-85 mx-auto">
         <div className="flex flex-col justify-between h-full">
           <div>
-            <div className="text-2xl pretendard-bold mb-1">닉네임 님</div>
+            <div className="text-2xl pretendard-bold mb-1">{nickName} 님</div>
             <div className="text-2xl pretendard-bold text-blue01">오늘도 즐수하세요!</div>
           </div>
-          <div>swim-done@kakao.com</div>
+          <div>{emain}</div>
         </div>
-        <div>
-          <div className="h-17 w-20 rounded-full overflow-hidden flex items-center justify-center">
-            <img src={profile} alt="" className="h-full w-full" />
-          </div>
-          <div className=" text-center cursor-pointer">로그아웃</div>
+        <div className="w-20 h-18">
+          <img src={userImg === '' ? profile : userImg} alt="" className="h-full w-full" />
+          <button className="w-full text-center cursor-pointer outline-none" onClick={logout}>
+            로그아웃
+          </button>
         </div>
       </div>
       <div className="relative top-30 h-80 w-85 mx-auto flex flex-col justify-between">
@@ -44,8 +57,10 @@ export default function MyPage() {
           }
         />
       </div>
-      <div className="relative top-67 h-10 w-85 mx-auto flex justify-center items-center">
-        <span className="cursor-pointer">회원 탈퇴하기</span>
+      <div className="flex justify-center">
+        <button className="relative top-69 h-10 cursor-pointer outline-none" onClick={removeUser}>
+          회원 탈퇴하기
+        </button>
       </div>
     </div>
   );
