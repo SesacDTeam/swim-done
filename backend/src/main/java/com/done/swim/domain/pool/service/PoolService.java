8f3848b3b203ceb61055c48d4bddc36e17d4d74c
lxodud <xodud1998223@naver.com>
2025-03-25 11:38:10 +0900

    [SW-65] refactor: PoolService 예외 수정

--- backend/src/main/java/com/done/swim/domain/pool/service/PoolService.java
+++ backend/src/main/java/com/done/swim/domain/pool/service/PoolService.java
@@ -4,6 +4,8 @@ import com.done.swim.domain.pool.dto.responsedto.PoolDetailResponseDto;
 import com.done.swim.domain.pool.dto.responsedto.PoolResponseDto;
 import com.done.swim.domain.pool.entity.Pool;
 import com.done.swim.domain.pool.repository.PoolRepository;
+import com.done.swim.global.exception.ErrorCode;
+import com.done.swim.global.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,20 +17,19 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class PoolService {
 
-    private final PoolRepository poolRepository;
+  private final PoolRepository poolRepository;
 
-    public PoolDetailResponseDto fetchPoolDetail(Long poolId) {
-        Pool pool = poolRepository.findByIdWithCommentAndTimes(poolId)
-                //이 후 GlobalException 정의 후 수정 예정
-                .orElseThrow(() -> new IllegalArgumentException("해당 수영장이 존재하지 않습니다."));
-        return PoolDetailResponseDto.from(pool);
-    }
+  public PoolDetailResponseDto fetchPoolDetail(Long poolId) {
+    Pool pool = poolRepository.findByIdWithCommentAndTimes(poolId)
+      .orElseThrow(() -> new ResourceNotFoundException(ErrorCode.POOL_NOT_FOUND));
+    return PoolDetailResponseDto.from(pool);
+  }
 
 
-    public List<PoolResponseDto> getPoolsWithUserMark(Long userId) {
-        List<Pool> pools = poolRepository.findAllWithUserMark(userId);
-        return pools.stream().map(p -> PoolResponseDto.from(p, userId)).toList();
+  public List<PoolResponseDto> getPoolsWithUserMark(Long userId) {
+    List<Pool> pools = poolRepository.findAllWithUserMark(userId);
+    return pools.stream().map(p -> PoolResponseDto.from(p, userId)).toList();
 
-    }
+  }
 
 }
