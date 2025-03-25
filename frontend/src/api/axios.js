bf2eef633eb7f32957dba6d5f8f742ab4c80988d
hisunny1007 <hisunny1007@gmail.com>
2025-03-25 10:32:10 +0900

    [SW-59] 리뷰 작성 페이지 구현 (#39)
    * [SW-59] feat: 리뷰 작성 html 구조 및 css 구현
    
    * [SW-59] refactor: input -> textarea로 변경
    
    - 한 줄 리뷰가 아니기 때문에 직관적으로 리뷰 작성을 보여주기 위해 textarea로 변경
    - 그에 맞는 css 적용
    
    * [SW-59] feat: reviewApi 구현
    
    - reviewApi 추가
    - axios.js 변수명 통일 (api -> instance)
    
    * [SW-59] refactor: 수영장 제목 가져오는 방법 수정 (uselocation 사용)
    
    - PoolDetail의 리뷰 쓰기 버튼 Link에 상태(state) 추가
    - CreateReview에서 state 전달받음 (uselocation 사용)
    
    * [SW-59] feat: 리뷰 작성 내용 state에 저장
    
    - reviewContent 상태 관리
    
    * [SW-59] design: 리뷰 미작성시 버튼 비활성화
    
    - 버튼 색상, 커서 모양 변경
    
    * [SW-59] fix: reviewContent도 요청에 포함

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import authApi from './authApi';
 
 const baseURL = import.meta.env.VITE_API_URL;
 
-const api = axios.create({
+const instance = axios.create({
   baseURL: baseURL, //api
   timeout: 5000,
   withCredentials: true, // 쿠키를 요청에 포함시키도록 설정함
@@ -11,7 +11,7 @@ const api = axios.create({
 
 
 // 요청 인터셉터 (api 요청할 때 가로채는 것임)
-api.interceptors.request.use(
+instance.interceptors.request.use(
   (config) => {
     const accessToken = localStorage.getItem('accessToken');
     if (accessToken) {
@@ -26,7 +26,7 @@ api.interceptors.request.use(
 );
 
 // 응답 인터셉터 (401 에러 발생 시 리프레시 토큰으로 액세스 토큰 재발급)
-api.interceptors.response.use(
+instance.interceptors.response.use(
   (response) => {
     return response;
   },
@@ -48,7 +48,7 @@ api.interceptors.response.use(
         originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
 
         // 실패한 요청 재시도
-        return api(originalRequest);
+        return instance(originalRequest);
 
       } catch (error) {
         await authApi.logout();
@@ -61,4 +61,4 @@ api.interceptors.response.use(
   },
 );
 
-export default api;
+export default instance;
