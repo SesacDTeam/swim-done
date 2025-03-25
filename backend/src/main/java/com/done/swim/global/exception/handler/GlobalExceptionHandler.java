823f7baa00dbaa962213addefae87c754d6a1389
lxodud <xodud1998223@naver.com>
2025-03-25 11:22:08 +0900

    [SW-65] feat: Bean Validation 예외 handler 구현

--- backend/src/main/java/com/done/swim/global/exception/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/done/swim/global/exception/handler/GlobalExceptionHandler.java
@@ -4,8 +4,12 @@ import com.done.swim.common.ApiResponse;
 import com.done.swim.global.exception.ErrorCode;
 import com.done.swim.global.exception.GlobalException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -16,6 +20,25 @@ public class GlobalExceptionHandler {
     return ResponseEntity
       .status(errorCode.getStatus())
       .body(ApiResponse.error(errorCode.getMessage(), errorCode.getCode()));
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ApiResponse<Object>> methodArgumentNotValidHandler(
+    MethodArgumentNotValidException exception) {
+
+    List<String> errorMessages = exception.getBindingResult().getFieldErrors()
+      .stream()
+      .map(FieldError::getDefaultMessage)
+      .toList();
+
+    String errorMessage = errorMessages.isEmpty()
+      ? ErrorCode.INVALID_INPUT.getMessage()
+      : errorMessages.getFirst();
+
+    return ResponseEntity
+      .status(ErrorCode.INVALID_INPUT.getStatus())
+      .body(ApiResponse.error(errorMessage,
+        ErrorCode.INVALID_INPUT.getCode()));
   }
 
 }
