5237af8b83eafb5b740c9dcf29495bd4449b7838
JaeHyeon_Lee <project0858@gmail.com>
2025-03-25 20:29:07 +0900

    [SW-9] refactor: 로그 추가

--- frontend/src/components/kakaomap/KakaoMapContainer.jsx
+++ frontend/src/components/kakaomap/KakaoMapContainer.jsx
@@ -164,6 +164,8 @@ export default function KakaoMapContainer() {
       polygon.setOptions({ fillColor: '#fff' });
       try {
         const { data: pools } = await kakaoMapApi.getSectionWithPools(name);
+        console.log(pools);
+
         const markers = pools.map(({ latitude, longitude, name }) =>
           createMarker(new kakao.maps.LatLng(latitude, longitude), name),
         );
