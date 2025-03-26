144744799d7797eab1c3525ebc77ba2caf2e3ecc
hisunny1007 <hisunny1007@gmail.com>
2025-03-26 14:04:31 +0900

    [SW-71] feat: 이미지 제보 페이지 구현

---
+++ frontend/src/components/common/submittedImage/SubmittedImage.jsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useRef, useState } from 'react';
+import DetailViewHeader from '../DetailViewHeader';
+import { xmark, back } from '../../../utils/staticImagePath';
+import { useLocation, useNavigate, useParams } from 'react-router';
+import submittedImageApi from '../../../api/submittedImageApi';
+import { defaultUploadImage } from '../../../utils/staticImagePath';
+
+export default function SubmittedImage() {
+  const { poolId } = useParams();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const poolName = location.state?.poolName;
+
+  const [inputData, setInputData] = useState({ file: null });
+  const [previewImage, setPreviewImage] = useState(null);
+
+  const fileInputRef = useRef(null);
+
+  // 파일 선택 버튼 클릭 시 input 클릭 이벤트 발생함 (input 숨겨놓음)
+  const handleButtonClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+
+    if (file) {
+      setInputData({ file });
+
+      if (previewImage) {
+        URL.revokeObjectURL(previewImage);
+      }
+
+      setPreviewImage(URL.createObjectURL(file));
+    }
+  };
+
+  // 컴포넌트가 언마운트될 때 미리보기 url 정리함
+  useEffect(() => {
+    return () => {
+      if (previewImage) {
+        URL.revokeObjectURL(previewImage);
+      }
+    };
+  }, [previewImage]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!inputData.file) {
+      alert('파일을 선택해 주세요!');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', inputData.file);
+    formData.append('poolId', poolId);
+
+    try {
+      await submittedImageApi.createImage(formData);
+      navigate(`/mark-pools/${poolId}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <main className="flex flex-col items-center w-full">
+        <DetailViewHeader backButtonImage={back} closeButtonImage={xmark}></DetailViewHeader>
+        <section className="w-[80%] flex flex-col items-center mb-10">
+          <h1 className="pretendard-bold text-3xl">{poolName}</h1>
+          <p className="pretendard-medium text-body01 text-2xl mb-10 mt-10">
+            수정하실 시간표 이미지를 첨부해 주세요.
+          </p>
+        </section>
+
+        {/* TODO : 미리보기 이미지 눌러도 파일 선택하게 해놨는데 굳이 파일선택 버튼이 그럼 필요있나? 미리보기 이미지에 + 버튼 있는게 더 좋지 않을까? */}
+        <div
+          className="w-90 h-60 flex justify-center items-center border border-gray02 rounded-lg bg-gray-200 cursor-pointer"
+          onClick={handleButtonClick}
+        >
+          {previewImage ? (
+            <img
+              src={previewImage}
+              alt="업로드된 이미지"
+              className="w-full h-full object-cover rounded-lg"
+            />
+          ) : (
+            <img src={defaultUploadImage} alt="기본 이미지" className="w-15 h-15 opacity-50" />
+          )}
+        </div>
+
+        {/* 제출 영역임 */}
+        <form onSubmit={handleSubmit} className="flex flex-col items-center mt-8 gap-4">
+          <div className="flex gap-4">
+            <button
+              className="pretendard-medium text-xl rounded-[10px] px-6 py-3 bg-blue02/10"
+              type="button"
+              onClick={handleButtonClick}
+            >
+              파일 선택
+            </button>
+
+            <button
+              className="pretendard-medium text-xl rounded-[10px] px-6 py-3 bg-blue01 text-white"
+              type="submit"
+            >
+              제출하기
+            </button>
+          </div>
+
+          <input
+            type="file"
+            ref={fileInputRef}
+            hidden // 기존의 input file 태그 숨김
+            onChange={handleFileChange}
+            accept="image/*"
+          />
+        </form>
+      </main>
+    </>
+  );
+}
