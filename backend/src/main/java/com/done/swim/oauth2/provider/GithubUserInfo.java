28ac21c965dc41e2a9de003ca256319dc6f8b902
hisunny1007 <hisunny1007@gmail.com>
2025-03-27 16:46:44 +0900

    [SW-77] feat: 깃허브 로그인 백엔드 구현

---
+++ backend/src/main/java/com/done/swim/oauth2/provider/GithubUserInfo.java
@@ -0,0 +1,48 @@
+package com.done.swim.oauth2.provider;
+
+import lombok.Setter;
+
+import java.util.Map;
+
+public class GithubUserInfo implements OAuth2UserInfo {
+
+    private Map<String, Object> attributes;
+
+    @Setter
+    private String email;
+
+    public GithubUserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+        this.email = (String) attributes.get("email"); // GitHub에서 제공하는 이메일 값 (없을 수도 있음)
+    }
+
+    @Override
+    public Provider getProvider() {
+        return Provider.GITHUB;
+    }
+
+    // github는 id가 Integer라 string으로 변환해야 함
+    // 근데 (String) attributes.get("id)는 Integer -> String 변환 시 ClassCastException 발생 가능함
+    // 에러 내용 : Integer cannot be cast to class java.lang.String
+    @Override
+    public String getProviderId() {
+        return String.valueOf(attributes.get("id"));
+    }
+
+    @Override
+    public String getEmail() {
+//        return (String) attributes.get("email");
+        return email;
+    }
+
+    @Override
+    public String getNickname() {
+        return (String) attributes.get("name");
+    }
+
+    @Override
+    public String getUserImageUrl() {
+        return "";
+    }
+
+}
