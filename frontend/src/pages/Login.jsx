33da1116f549f4c72d26725570e9452cb168a015
hisunny1007 <hisunny1007@gmail.com>
2025-03-28 11:36:43 +0900

    [SW-80] design: 홈으로 가기 버튼 위치 조정
    - 깃허브 버튼 추가 후 홈으로 가기 텍스트 짤려서 위치 조정

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -10,7 +10,7 @@ export default function Login() {
   return (
     <>
         {/* <img src={logoMan} alt="로고 이미지" className="ml-30 mt-5 w-20" /> */}
-        <div className="font-pretendard font-bold text-center text-5xl mt-35 mb-10 text-title">
+        <div className="font-pretendard font-bold text-center text-5xl mt-30 mb-10 text-title">
         <h1 className="mb-5">
           <span className="text-blue01">자유 수영</span> 정보를 한눈에 확인하고
         </h1>
@@ -19,7 +19,7 @@ export default function Login() {
         </h1>
       </div>
         <div className='font-pretendard font-semibold text-center text-xl text-title/80'>
-          <p>자유 수영의 모든 것, 오수완에 오신 걸 환영해요!🏊‍♂️</p>
+          <p>🏊‍♂️ 자유 수영의 모든 것, 오수완에 오신 걸 환영해요 🏊‍♂️</p>
         </div>
       <div className="flex flex-col mt-15 space-y-3">
         <NaverLoginButton></NaverLoginButton>
@@ -27,7 +27,7 @@ export default function Login() {
         <GithubLoginButton></GithubLoginButton>
       </div>
       <Link to="/">
-        <div className="font-pretendard text-center text-gray04 mt-30">🏠 홈으로 가기</div>
+        <div className="font-pretendard text-center text-gray04 mt-20">🏠 홈으로 가기</div>
       </Link>
     </>
   );
