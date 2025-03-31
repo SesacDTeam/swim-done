3dec425c20869383c27d348d168dae79c27b9e56
ssdeok <bechu123456@gmail.com>
2025-03-31 17:42:07 +0900

    [SW-98] chore: 토글 로직 추가

--- frontend/src/hooks/useToggleMark.jsx
+++ frontend/src/hooks/useToggleMark.jsx
@@ -1,14 +1,20 @@
 import { markPoolApi } from '../api/markPoolApi';
 import ERROR_DISPLAY_MODE from '../error/ERROR_DISPLAY_MODE';
 import useErrorResolver from './useErrorResolver';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router';
 
 export const useToggleMark = () => {
   const { setError } = useErrorResolver(ERROR_DISPLAY_MODE.TOAST);
-
+  const navigate = useNavigate();
+  const isLoggedIn = useSelector((state) => {
+    return state.auth.isLoggedIn;
+  });
   const toggleMark = async (index, pools, setPools) => {
+    if (!isLoggedIn) {
+      navigate('/login');
+    }
     const isMarked = pools[index].mark;
-
-    // 로그인 상태인지 아닌지 useselector
 
     try {
       if (isMarked) {
