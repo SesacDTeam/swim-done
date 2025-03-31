9a9bc641fffe1a29338e0b546ef726fc19b7799b
hisunny1007 <hisunny1007@gmail.com>
2025-03-31 20:40:58 +0900

    [SW-99] feat: alertmodal창 확인 /취소 버튼 두가지 있는 버전으로 추가

--- frontend/src/components/common/AlertModal.jsx
+++ frontend/src/components/common/AlertModal.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 
-export default function AlertModal({ message, onClose }) {
+export default function AlertModal({
+  isSingleButton = false, // 확인 버튼만 있을 때
+  message,
+  onCancel,
+  onConfirm,
+}) {
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 bg-gray03/60 flex justify-center items-center z-10000">
       <div className="bg-white w-100 h-50 rounded-2xl flex flex-col items-center justify-between pt-6 overflow-hidden">
         <div className="mt-10 text-center pretendard-medium">
-          <div>{message}</div>
+          {Array.isArray(message) ? (
+            message.map((line, index) => <div key={index}>{line}</div>)
+          ) : (
+            <div>{message}</div>
+          )}
         </div>
         <div className="flex w-full h-12">
-          <button className="flex-1 bg-blue01 text-white" onClick={onClose}>
+          {!isSingleButton && onCancel && (
+            <button className="flex-1 bg-gray03 text-white w-full" onClick={onCancel}>
+              취소
+            </button>
+          )}
+          <button className="flex-1 bg-blue01 text-white w-full" onClick={onConfirm}>
             확인
           </button>
         </div>
