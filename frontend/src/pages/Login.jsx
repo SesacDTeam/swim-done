4c9a584518cef4af42311145e29ef4fab8829e30
hisunny1007 <hisunny1007@gmail.com>
2025-03-31 15:20:58 +0900

    [SW-94] 최근 로그인 기능 구현 (#68)
    * [SW-94] feat: 로그인 이력 배열로 저장
    
    - 리다이렉트 url에 provider 추가
    - 로그인 이력 배열로 저장
    
    * [SW-94] fix: 로그아웃 후 로그인 시 로그인 이력 초기화 수정
    
    * [SW-94] feat: 최근 로그인 표시 말풍선 추가

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,11 +3,19 @@ import KakaoLoginButton from '../components/sociallogin/KakaoLoginButton';
 import NaverLoginButton from '../components/sociallogin/NaverLoginButton';
 import GithubLoginButton from '../components/sociallogin/GithubLoginButton';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { login } from '../store/slices/authSlice';
 
 export default function Login() {
+  // 리덕스에서 로그인 이력 가져오기
+  const loginHistory = useSelector((state) => state.auth.loginHistory);
+  // 최근 로그인한 provider 가져오기
+  const recentProvider =
+    loginHistory.length > 0 ? loginHistory[loginHistory.length - 1].provider : null;
+
   return (
     <>
-      <div className="font-bold text-center text-5xl mt-30 mb-10 text-title">
+      <div className="font-pretendard font-bold text-center text-5xl mt-40 mb-10 text-title">
         <h1 className="mb-5">
           <span className="text-blue01">자유 수영</span>의 모든 것
         </h1>
@@ -15,16 +23,57 @@ export default function Login() {
           <span className="text-blue01">오수완</span>에 오신 걸 환영해요!
         </h1>
       </div>
-      <div className="font-semibold text-center text-xl text-title/80">
+      <div className="font-pretendard font-semibold text-center text-xl text-title/80">
         <p>자유 수영 정보를 한눈에 확인하고 내가 원하는 시간에 수영을 즐겨보세요 🏊‍♂️</p>
       </div>
-      <div className="flex flex-col mt-15 space-y-3">
-        <KakaoLoginButton></KakaoLoginButton>
-        <NaverLoginButton></NaverLoginButton>
-        <GithubLoginButton></GithubLoginButton>
+
+      {/* 각 버튼 위에 최근 로그인 정보 표시 */}
+      <div className="flex flex-col mt-17 space-y-3 font-semibold items-center">
+        {/* 카카오 로그인 */}
+        <div className="relative w-fit flex">
+          {recentProvider === 'KAKAO' && (
+            <div className="absolute top-[-20px] left-30 bg-white text-title border border-gray02 text-xs px-3 py-1 rounded-md shadow-md">
+              최근 로그인
+              <div
+                className="absolute left-1/2 -translate-x-1/2 bottom-[-6px] w-4 h-3 bg-white border-l border-r border-gray02"
+                style={{ clipPath: 'polygon(50% 100%, 0% 0%, 100% 0%)' }}
+              ></div>
+            </div>
+          )}
+          <KakaoLoginButton />
+        </div>
+
+        {/* 네이버 로그인 */}
+        <div className="relative w-fit flex">
+          {recentProvider === 'NAVER' && (
+            <div className="absolute top-[-20px] left-30 bg-white text-title border border-gray02 text-xs px-3 py-1 rounded-md shadow-md">
+              최근 로그인
+              <div
+                className="absolute left-1/2 -translate-x-1/2 bottom-[-6px] w-4 h-3 bg-white border-l border-r border-gray02"
+                style={{ clipPath: 'polygon(50% 100%, 0% 0%, 100% 0%)' }}
+              ></div>
+            </div>
+          )}
+          <NaverLoginButton />
+        </div>
+
+        {/* 깃허브 로그인 */}
+        <div className="relative">
+          <GithubLoginButton />
+          {recentProvider === 'GITHUB' && (
+            <div className="absolute top-[-20px] left-30 bg-white text-title border border-gray02 text-xs px-3 py-1 rounded-md shadow-md">
+              최근 로그인
+              <div
+                className="absolute left-1/2 -translate-x-1/2 bottom-[-6px] w-4 h-3 bg-white border-l border-r border-gray02"
+                style={{ clipPath: 'polygon(50% 100%, 0% 0%, 100% 0%)' }}
+              ></div>
+            </div>
+          )}
+        </div>
       </div>
+
       <Link to="/">
-        <div className="text-center text-gray04 mt-20">🏠 홈으로 가기</div>
+        <div className="font-pretendard text-center text-gray04 mt-15">🏠 홈으로 가기</div>
       </Link>
     </>
   );
--- frontend/src/store/slices/authSlice.js
+++ frontend/src/store/slices/authSlice.js
@@ -3,6 +3,9 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
   accessToken: localStorage.getItem('accessToken'),
   isLoggedIn: !!localStorage.getItem('accessToken'),
+  // 로그인 이력 표시하기 위해 소셜 로그인 서비스 정보랑 로그인 이력 추가
+  provider: localStorage.getItem('provider'),
+  loginHistory: JSON.parse(localStorage.getItem('loginHistory')) || [],
 };
 
 const authSlice = createSlice({
@@ -10,14 +13,29 @@ const authSlice = createSlice({
   initialState,
   reducers: {
     login: (state, action) => {
-      state.accessToken = action.payload;
+      const { accessToken, provider } = action.payload
+      state.accessToken = accessToken;
+      state.provider = provider;
       state.isLoggedIn = true;
-      localStorage.setItem('accessToken', action.payload);
+
+      // 기존 이력 유지하면서 새로운 로그인 기록 추가함
+      const newHistory = [
+        ...state.loginHistory,
+        { provider, timestamp: new Date().toISOString() }
+      ];
+      state.loginHistory = newHistory;
+
+      localStorage.setItem('accessToken', accessToken);
+      localStorage.setItem('provider', provider);
+      localStorage.setItem('loginHistory', JSON.stringify(newHistory)); // 배열로 로그인 이력 저장
     },
     logout: (state) => {
       state.accessToken = null;
+      state.provider = null;
       state.isLoggedIn = false;
+
       localStorage.removeItem('accessToken');
+      localStorage.removeItem('provider');
     },
   },
 });
