29ffa482fe7d062bce92fc4f17304262e00b1b27
hisunny1007 <hisunny1007@gmail.com>
2025-04-04 01:27:38 +0900

    [SW-134] feat: 메인 화면 배너 추가
    - 메인 화면에서만 배너 띄움 (다른 페이지에서는 x)

---
+++ frontend/src/components/common/MainBanner.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function MainBanner() {
+  return (
+    <div className="w-full bg-blue02/20 text-title text-center text-lg py-3 font-semibold shadow-md animate-fade-in">
+      🏊🏻‍♂️ 원하는 구를 클릭하면 해당 지역의 수영장을 확인할 수 있어요!
+    </div>
+  );
+}
