64337b37f602fc77a96f06cb5b699e358c0229a2
hisunny1007 <hisunny1007@gmail.com>
2025-04-03 15:25:03 +0900

    [SW-129] fix: 쿠키 없을 때 에러 응답코드 수정
    - GlobalExceptionHandler => INVALID_REFRESH_TOKEN

--- backend/src/main/java/com/done/swim/global/exception/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/done/swim/global/exception/handler/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package com.done.swim.global.exception.handler;
 import com.done.swim.common.ApiResponse;
 import com.done.swim.global.exception.ErrorCode;
 import com.done.swim.global.exception.GlobalException;
-import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
@@ -14,6 +13,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
+import java.util.List;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -22,61 +23,61 @@ public class GlobalExceptionHandler {
         ErrorCode errorCode = exception.getErrorCode();
 
         return ResponseEntity
-            .status(errorCode.getStatus())
-            .body(ApiResponse.error(errorCode.getMessage(), errorCode.getCode()));
+                .status(errorCode.getStatus())
+                .body(ApiResponse.error(errorCode.getMessage(), errorCode.getCode()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiResponse<Void>> methodArgumentNotValidHandler(
-        MethodArgumentNotValidException exception) {
+            MethodArgumentNotValidException exception) {
 
         List<String> errorMessages = exception.getBindingResult().getFieldErrors()
-            .stream()
-            .map(FieldError::getDefaultMessage)
-            .toList();
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .toList();
 
         String errorMessage = errorMessages.isEmpty()
-            ? ErrorCode.INVALID_INPUT.getMessage()
-            : errorMessages.getFirst();
+                ? ErrorCode.INVALID_INPUT.getMessage()
+                : errorMessages.getFirst();
 
         return ResponseEntity
-            .status(ErrorCode.INVALID_INPUT.getStatus())
-            .body(ApiResponse.error(errorMessage,
-                ErrorCode.INVALID_INPUT.getCode()));
+                .status(ErrorCode.INVALID_INPUT.getStatus())
+                .body(ApiResponse.error(errorMessage,
+                        ErrorCode.INVALID_INPUT.getCode()));
     }
 
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     public ResponseEntity<ApiResponse<Object>> maxUploadSizeExceededHandler(
-        MaxUploadSizeExceededException exception) {
+            MaxUploadSizeExceededException exception) {
         return ResponseEntity.status(ErrorCode.PAYLOAD_TOO_LARGE.getStatus())
-            .body(ApiResponse
-                .error(ErrorCode.PAYLOAD_TOO_LARGE.getMessage(),
-                    ErrorCode.PAYLOAD_TOO_LARGE.getCode()));
+                .body(ApiResponse
+                        .error(ErrorCode.PAYLOAD_TOO_LARGE.getMessage(),
+                                ErrorCode.PAYLOAD_TOO_LARGE.getCode()));
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<ApiResponse<Object>> methodArgumentTypeMismatchHandler(
-        MethodArgumentTypeMismatchException exception) {
+            MethodArgumentTypeMismatchException exception) {
         return ResponseEntity.status(ErrorCode.INVALID_REQUEST.getStatus())
-            .body(ApiResponse
-                .error(ErrorCode.INVALID_REQUEST.getMessage(),
-                    ErrorCode.INVALID_REQUEST.getCode()));
+                .body(ApiResponse
+                        .error(ErrorCode.INVALID_REQUEST.getMessage(),
+                                ErrorCode.INVALID_REQUEST.getCode()));
     }
 
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<ApiResponse<Object>> authenticationHandler(
-        AuthenticationException exception) {
+            AuthenticationException exception) {
         return ResponseEntity.status(ErrorCode.INVALID_ACCESS_TOKEN.getStatus())
-            .body(ApiResponse
-                .error(ErrorCode.INVALID_ACCESS_TOKEN.getMessage(),
-                    ErrorCode.INVALID_ACCESS_TOKEN.getCode()));
+                .body(ApiResponse
+                        .error(ErrorCode.INVALID_ACCESS_TOKEN.getMessage(),
+                                ErrorCode.INVALID_ACCESS_TOKEN.getCode()));
     }
-  
+
     @ExceptionHandler(MissingRequestCookieException.class)
     public ResponseEntity<ApiResponse<Object>> missingRequestCookieHandler() {
-        return ResponseEntity.status(ErrorCode.INVALID_REQUEST.getStatus())
-            .body(ApiResponse
-                .error(ErrorCode.INVALID_REQUEST.getMessage(),
-                    ErrorCode.INVALID_REQUEST.getCode()));
+        return ResponseEntity.status(ErrorCode.INVALID_REFRESH_TOKEN.getStatus())
+                .body(ApiResponse
+                        .error(ErrorCode.INVALID_REFRESH_TOKEN.getMessage(),
+                                ErrorCode.INVALID_REFRESH_TOKEN.getCode()));
     }
 }
