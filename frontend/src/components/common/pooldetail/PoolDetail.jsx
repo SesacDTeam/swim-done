d2b0346fbbe39909a0a8c4e677a06e7c98fa6520
hisunny1007 <hisunny1007@gmail.com>
2025-04-04 02:07:35 +0900

    [SW-134] design: 커서 포인터 추가
    - 확인 모달창, 리뷰 작성 / 이미지 제보 버튼에 커서 포인터 추가

--- frontend/src/components/common/pooldetail/PoolDetail.jsx
+++ frontend/src/components/common/pooldetail/PoolDetail.jsx
@@ -75,7 +75,7 @@ export default function PoolDetail() {
           </div>
         </div>
         <Link to="submitted-images" className="self-end" state={{ poolName: poolDetail?.name }}>
-          <button className="bg-blue01 w-30 h-10 rounded-[10px] text-white pretendard-normal text-sm">
+          <button className="bg-blue01 w-30 h-10 rounded-[10px] text-white pretendard-normal text-sm cursor-pointer">
             수정 제안하기
           </button>
         </Link>
@@ -89,7 +89,7 @@ export default function PoolDetail() {
       <section className="flex flex-col items-center w-[80%] mb-10">
         <h2 className="self-start pretendard-semibold text-2xl">리뷰</h2>
         <Link to="reviews" className="self-end" state={{ poolName: poolDetail?.name }}>
-          <button className="text-white bg-blue01 w-30 h-10 rounded-[10px] pretendard-normal text-sm">
+          <button className="text-white bg-blue01 w-30 h-10 rounded-[10px] pretendard-normal text-sm cursor-pointer">
             리뷰 쓰러가기
           </button>
         </Link>
